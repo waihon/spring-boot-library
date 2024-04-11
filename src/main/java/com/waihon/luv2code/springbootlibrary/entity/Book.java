da5e5b608e0ab71149711ea3d373660f785f7418
Waihon Yew <yewwaihon@gmail.com>
2024-04-11 14:53:45 +0800

    Create Book entity class

---
+++ src/main/java/com/waihon/luv2code/springbootlibrary/entity/Book.java
@@ -0,0 +1,35 @@
+package com.waihon.luv2code.springbootlibrary.entity;
+
+import lombok.Data;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copiesAvailable")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+}
