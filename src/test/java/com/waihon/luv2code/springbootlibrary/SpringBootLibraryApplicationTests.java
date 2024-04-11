a6362ff7b89c8b4753c459e06c062430ce22e887
Waihon Yew <yewwaihon@gmail.com>
2024-04-11 09:21:48 +0800

    Initialize project using Spring Initializr

---
+++ src/test/java/com/waihon/luv2code/springbootlibrary/SpringBootLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waihon.luv2code.springbootlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
