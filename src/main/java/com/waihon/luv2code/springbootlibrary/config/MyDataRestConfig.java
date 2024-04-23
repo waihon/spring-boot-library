d2bf446b8dc0c7c5d0b9f36977e6eda28cbfd6fb
Waihon Yew <yewwaihon@gmail.com>
2024-04-24 00:03:58 +0800

    Configure Data REST for Review

--- src/main/java/com/waihon/luv2code/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/waihon/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.waihon.luv2code.springbootlibrary.config;
 
 import com.waihon.luv2code.springbootlibrary.entity.Book;
+import com.waihon.luv2code.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -21,8 +22,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 HttpMethod.DELETE};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
