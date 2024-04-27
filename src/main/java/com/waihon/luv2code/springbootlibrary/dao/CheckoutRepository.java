9502d18e6292cd713d23ae2343ff95081b1d9d0b
Waihon Yew <yewwaihon@gmail.com>
2024-04-28 06:51:02 +0800

    Create Checkout Repository

---
+++ src/main/java/com/waihon/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -0,0 +1,9 @@
+package com.waihon.luv2code.springbootlibrary.dao;
+
+import com.waihon.luv2code.springbootlibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+}
