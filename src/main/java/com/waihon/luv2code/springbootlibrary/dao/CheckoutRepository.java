2ea891a9be4f961c6443d328bdbc3b06747fc059
Waihon Yew <yewwaihon@gmail.com>
2024-04-28 09:48:33 +0800

    Find checked out books by user email

--- src/main/java/com/waihon/luv2code/springbootlibrary/dao/CheckoutRepository.java
+++ src/main/java/com/waihon/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -3,7 +3,11 @@ package com.waihon.luv2code.springbootlibrary.dao;
 import com.waihon.luv2code.springbootlibrary.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
 
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    List<Checkout> findBooksByUserEmail(String userEmail);
 }
