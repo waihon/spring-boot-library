d654f12422d0ebceb768ad5b65490bab0b496acc
Waihon Yew <yewwaihon@gmail.com>
2024-04-28 23:10:38 +0800

    Utility to extract sub value from the payload of JWT

---
+++ src/main/java/com/waihon/luv2code/springbootlibrary/utils/ExtractJWT.java
@@ -0,0 +1,44 @@
+package com.waihon.luv2code.springbootlibrary.utils;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExtractJWT {
+
+    public static String payloadJWTExtraction(String token) {
+
+        token.replace("Bearer ", "");
+
+        // Split by period into Header, Payload, Signature.
+        String[] chunks = token.split("\\.");
+
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String payload = new String(decoder.decode(chunks[1]));
+
+        String[] entries = payload.split(",");
+        Map<String, String> map = new HashMap<>();
+
+        for (String entry : entries) {
+            String[] keyValue = entry.split(":");
+            if (keyValue[0].equals("\"sub\"")) {
+                int remove = 1;
+                if (keyValue[1].endsWith("}")) {
+                    remove = 2;
+                }
+                System.out.println("0: " + keyValue[1]);
+                keyValue[1] = keyValue[1].substring(0, keyValue[1].length() - remove);
+                System.out.println("1: " + keyValue[1]);
+                keyValue[1] = keyValue[1].substring(1);
+                System.out.println("1: " + keyValue[1]);
+
+                map.put(keyValue[0], keyValue[1]);
+            }
+        }
+        if (map.containsKey("\"sub\"")) {
+            return map.get("\"sub\"");
+        }
+
+        return null;
+    }
+}
