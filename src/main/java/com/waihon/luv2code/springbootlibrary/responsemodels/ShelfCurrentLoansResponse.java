4fa4b7e4f5d96ea22b8f8ee058da777826ea14c2
Waihon Yew <yewwaihon@gmail.com>
2024-05-05 11:37:48 +0800

    Create Current Loans response model

---
+++ src/main/java/com/waihon/luv2code/springbootlibrary/responsemodels/ShelfCurrentLoansResponse.java
@@ -0,0 +1,17 @@
+package com.waihon.luv2code.springbootlibrary.responsemodels;
+
+import com.waihon.luv2code.springbootlibrary.entity.Book;
+import lombok.Data;
+
+@Data
+public class ShelfCurrentLoansResponse {
+
+    public ShelfCurrentLoansResponse(Book book, int daysLeft) {
+        this.book = book;
+        this.daysLeft = daysLeft;
+    }
+
+    private Book book;
+
+    private int daysLeft;
+}
