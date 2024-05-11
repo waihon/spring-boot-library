6524b5b324b5efc016a0085a1b097cbae6f20802
Waihon Yew <yewwaihon@gmail.com>
2024-05-11 20:38:46 +0800

    Create Messages controller with add message endpoint

---
+++ src/main/java/com/waihon/luv2code/springbootlibrary/controller/MessagesController.java
@@ -0,0 +1,27 @@
+package com.waihon.luv2code.springbootlibrary.controller;
+
+import com.waihon.luv2code.springbootlibrary.entity.Message;
+import com.waihon.luv2code.springbootlibrary.service.MessagesService;
+import com.waihon.luv2code.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesController {
+
+    private MessagesService messagesService;
+
+    @Autowired
+    public MessagesController(MessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token,
+                            @RequestBody Message messageRequest) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messagesService.postMessage(messageRequest, userEmail);
+    }
+}
