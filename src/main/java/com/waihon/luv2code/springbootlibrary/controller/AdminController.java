5d622bafbfa5b2673c7cb6a885b2509733748a3f
Waihon Yew <yewwaihon@gmail.com>
2024-05-19 14:26:37 +0800

    Implement Increase Book Quantity endpoint

--- src/main/java/com/waihon/luv2code/springbootlibrary/controller/AdminController.java
+++ src/main/java/com/waihon/luv2code/springbootlibrary/controller/AdminController.java
@@ -18,6 +18,17 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value = "Authorization ") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+
+        adminService.increaseBookQuantity(bookId);
+    }
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value = "Authorization") String token,
                          @RequestBody AddBookRequest addBookRequest) throws Exception {
