f99e7f031acf4d4194cde3aff444b9c97ff00bec
Waihon Yew <yewwaihon@gmail.com>
2024-05-19 14:49:39 +0800

    Implement Decrease Book Quantity endpoint

--- src/main/java/com/waihon/luv2code/springbootlibrary/controller/AdminController.java
+++ src/main/java/com/waihon/luv2code/springbootlibrary/controller/AdminController.java
@@ -29,6 +29,17 @@ public class AdminController {
         adminService.increaseBookQuantity(bookId);
     }
 
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization ") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+
+        adminService.decreaseBookQuantity(bookId);
+    }
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value = "Authorization") String token,
                          @RequestBody AddBookRequest addBookRequest) throws Exception {
