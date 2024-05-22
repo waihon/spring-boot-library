f99629f129f6027fa13475b516d3590adec5066a
Waihon Yew <yewwaihon@gmail.com>
2024-05-22 10:50:25 +0800

    Fix extra space in Request Header

--- src/main/java/com/waihon/luv2code/springbootlibrary/controller/AdminController.java
+++ src/main/java/com/waihon/luv2code/springbootlibrary/controller/AdminController.java
@@ -19,7 +19,7 @@ public class AdminController {
     }
 
     @PutMapping("/secure/increase/book/quantity")
-    public void increaseBookQuantity(@RequestHeader(value = "Authorization ") String token,
+    public void increaseBookQuantity(@RequestHeader(value = "Authorization") String token,
                                      @RequestParam Long bookId) throws Exception {
         String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
         if (admin == null || !admin.equals("admin")) {
@@ -30,7 +30,7 @@ public class AdminController {
     }
 
     @PutMapping("/secure/decrease/book/quantity")
-    public void decreaseBookQuantity(@RequestHeader(value = "Authorization ") String token,
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token,
                                      @RequestParam Long bookId) throws Exception {
         String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
         if (admin == null || !admin.equals("admin")) {
