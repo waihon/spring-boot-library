7267215e7f9e8c918394fdd301e7adbde69a2513
Waihon Yew <yewwaihon@gmail.com>
2024-06-04 15:03:31 +0800

    Create Payment service

---
+++ src/main/java/com/waihon/luv2code/springbootlibrary/service/PaymentService.java
@@ -0,0 +1,21 @@
+package com.waihon.luv2code.springbootlibrary.service;
+
+import com.stripe.Stripe;
+import com.waihon.luv2code.springbootlibrary.dao.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PaymentService {
+
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.key.secret}") String secretKey) {
+        this.paymentRepository = paymentRepository;
+        Stripe.apiKey = secretKey;
+    }
+}
